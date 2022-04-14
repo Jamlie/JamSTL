770e545eb72b6e81d886b455f4ea259a7116e992
Omar <88511754+Jamlee977@users.noreply.github.com>
2022-04-14 07:35:02 +0300

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,65 @@
+class real {
+private:
+    double *number;
+public:
+    real(double value = 0):number{new double(value)} {};
+    void set(double value) { *number = value; }
+    void print() { cout << *number << '\n'; }
+    ~real() { delete number; }
+
+    real& operator= (const real &REAL) {
+        if (this != &REAL) *number = *REAL.number;
+        return *this; 
+    }
+
+    real operator+ (const real& REAL) const {
+        return real(*number + *REAL.number);
+	}
+
+    real operator* (const real& REAL) const {
+        return real(*number * *REAL.number);
+	}
+
+    real operator- (const real& REAL) const {
+        return real(*number - *REAL.number);
+	}
+
+    real operator/ (const real& REAL) const {
+        return real(*number / *REAL.number);
+	}
+
+    bool operator== (real REAL) {
+		return *number == *REAL.number;
+	}
+
+    bool operator!= (real REAL) {
+		return *number != *REAL.number;
+	}
+
+    bool operator> (real REAL) {
+		return *number > *REAL.number;
+	}
+
+    bool operator>= (real REAL) {
+		return *number >= *REAL.number;
+	}
+
+    bool operator< (real REAL) {
+		return *number < *REAL.number;
+	}
+
+    bool operator<= (real REAL) {
+		return *number <= *REAL.number;
+	}
+
+    friend ostream& operator<<(ostream& os, const real& REAL) {
+        os << *REAL.number;
+        return os;
+    }
+    friend istream& operator>>(istream& is, const real& REAL) {
+        is >> *REAL.number;
+        return is;
+    }
+
+
+};
